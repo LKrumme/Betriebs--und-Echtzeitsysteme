8664bcd86cfc429f67d69b973d4950f8dcc7b095
Louis Krumme <louis@krumme-naturgaerten.de>
2025-06-19 12:42:03 +0200

    Add player_logic

--- workspace/pong/src/pong.c
+++ workspace/pong/src/pong.c
@@ -16,10 +16,21 @@
 #define PLIC_ENABLE 0x2000
 
 //CONSTANTS
-#define STANDARD_WAIT_SEMAPHORE 10 //ms
+#define STANDARD_WAIT_SEMAPHORE 2 //ms
+#define UPDATE_RATE_BALL 20 //ms
+#define UPDATE_RATE_PLAYER 5 //ms
+#define PLAYER_STEP 1
 
-//Functions
+//Structs
+struct key_pres
+{
+	int button_pressed;
+	int button_num;
+} key_press_size, key_press_player, key_press_keyboard; //Drei variablen, damit diese nicht Daten untereinadner austauschen, falls etwas schiefgeht.
+
+//Tasks
 void ball_logic(void *pvParameters);
+void player_logic(void *pvParameters);
 
 //Variables
 
@@ -50,6 +61,8 @@ const float fWidthPlayer2Y;
 SemaphoreHandle_t xDraw_screen;
 SemaphoreHandle_t xScore_Update_Mutex;
 QueueHandle_t xScore_Update;
+SemaphoreHandle_t xKey_Queue_Mutex;
+QueueHandle_t xKey_Queue;
 
 // interrupt handler for handling all unclaimed interrupts
 void irq_handler(void)
@@ -69,10 +82,14 @@ int main( void )
 	xDraw_screen = xSemaphoreCreateBinary();
 	xScore_Update = xQueueCreate(2, sizeof(int)); //Könnte Speicheroptimierung vertragen, da man für Zwei Spieler nur einen Byte braucht, aber er hat mich nicht machen lassen
 	xScore_Update_Mutex =xSemaphoreCreateMutex();
+	xKey_Queue = xQueueCreate(10, sizeof(key_press_size));
+	xKey_Queue_Mutex = xSemaphoreCreateMutex();
 	
-	if(xDraw_screen == NULL || xScore_Update == NULL || xScore_Update_Mutex == NULL){ //Kritischer Fehler. Programm kann ohne nicht laufen
+	if(xDraw_screen == NULL || xScore_Update == NULL || xScore_Update_Mutex == NULL || xKey_Queue == NULL || xKey_Queue_Mutex == NULL){ //Kritischer Fehler. Programm kann ohne nicht laufen
 		setEntireDisplayOn(1);
 	}
+
+	//TODO Tasks initialisieren
 
 	vTaskStartScheduler();
 
@@ -138,9 +155,81 @@ void ball_logic(void *pvParameters){
 			//Ressource genommen -> draw_screen kann Ressource jetzt freigeben
 		} 
 		else {
-			//Ressource konnte nicht genommen werden. Fehler, aber nicht schlimm, wenn der Bildschirm nicht geladen wurde.
+			//Ressource konnte nicht genommen werden. Fehler, aber nicht schlimm, wenn der Bildschirm nicht aktualisiert wurde.
 		}
-	}
+		vTaskDelay( pdMS_TO_TICKS( UPDATE_RATE_BALL ) );
+	}	
+}
 
+void player_logic(void *pvParameters){
+	//Internes speichern der Button-Zustände
+	int green_pressed;
+	int blue_pressed;
+	int yellow_pressed;
+	int red_pressed;
+	
+	for(;;){
+		if(xSemaphoreTake(xKey_Queue_Mutex, pdMS_TO_TICKS(STANDARD_WAIT_SEMAPHORE)==pdTRUE)){
+			if(xQueueReceive(xKey_Queue, &key_press_player, pdMS_TO_TICKS(STANDARD_WAIT_SEMAPHORE))==pdPASS){
+				xSemaphoreGive(xKey_Queue_Mutex);
+				//Button handeln
+				switch (key_press_player.button_num)
+				{
+				case 2:
+					green_pressed = key_press_player.button_pressed;
+					break;
+				case 3: 
+					blue_pressed = key_press_player.button_pressed;
+					break;
+				case 4:
+					yellow_pressed = key_press_player.button_pressed;
+					break;
+				case 5:
+					red_pressed = key_press_player.button_pressed;
+					break;
+				default:
+					break;
+				}
+			}
+			else{
+				xSemaphoreGive(xKey_Queue_Mutex);
+				//Queue ist leer. Nicht schlimm. Einfach weitermachen.
+			}
+		}
+
+		//Spieler1 bewegen: 
+		if(green_pressed){ //hoch
+			if(fPositionPlayer1Y+fWidthPlayer1Y < DISP_H){ //Out of Bounds check
+				fPositionPlayer1Y = fPositionPlayer1Y+PLAYER_STEP;
+			}
+		}
+		if(blue_pressed){ //runter
+			if(fPositionPlayer1Y-fWidthPlayer1Y > 0){
+				fPositionPlayer1Y = fPositionPlayer1Y-PLAYER_STEP;
+			}
+		}
+
+		//Spieler2 bewegen: 
+		if(red_pressed){ //hoch
+			if(fPositionPlayer2Y+fWidthPlayer2Y < DISP_H){
+				fPositionPlayer2Y = fPositionPlayer2Y+PLAYER_STEP;
+			}
+		}
+		if(yellow_pressed){ //runter
+			if(fPositionPlayer2Y-fWidthPlayer2Y > 0){
+				fPositionPlayer2Y = fPositionPlayer2Y+PLAYER_STEP;
+			}
+		}
+
+
+		//Tell draw_screen Task to do its Job
+		if(xSemaphoreTake(xDraw_screen, pdMS_TO_TICKS(STANDARD_WAIT_SEMAPHORE)==pdTRUE)){//Warten bis Ressource freigegeben wird
+			//Ressource genommen -> draw_screen kann Ressource jetzt freigeben
+		} 
+		else {
+			//Ressource konnte nicht genommen werden. Fehler, aber nicht schlimm, wenn der Bildschirm nicht aktualisiert wurde.
+		}
+		vTaskDelay( pdMS_TO_TICKS( UPDATE_RATE_PLAYER ) );
+	}
 
 }
